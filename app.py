e3683959ea0b4f64650f5df1da06c505392093ff
ignasand <ignas.andrijauskas@gmail.com>
2019-10-19 14:00:59 +0300

    text field

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DecimalField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
+
+class NameForm(FlaskForm):
+    name = StringField('Enter the number', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contacts', methods=['GET','POST'])
+def contacts():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('contacts.html', form=form, name=name)
 
 
 
