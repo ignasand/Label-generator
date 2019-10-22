71e5d31780e790e7be2bbe1ed7b8a0da7ba7b8f5
ignasand <ignas.andrijauskas@gmail.com>
2019-10-22 12:46:34 +0300

    function is added

--- app.py
+++ app.py
@@ -14,6 +14,12 @@ class NumberForm(FlaskForm):
     number2 = DecimalField('Enter second number', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
+def do_calculations(number, number2):
+    result = number / number2
+    return result
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -26,14 +32,16 @@ def about():
 def contacts():
     number = None
     number2 = None
+    result = None
     form = NumberForm()
     if form.validate_on_submit():
         number = form.number.data
         number2 = form.number2.data
         form.number.data = ''
         form.number2.data = ''
+        result = do_calculations(number, number2)
 
-    return render_template('contacts.html', form=form, number=number, number2=number2)
+    return render_template('contacts.html', form=form, number=number, number2=number2, result=result)
 
 
 
