33c9e92400b105fd2e8663a2322f06ff929d3520
ignasand <ignas.andrijauskas@gmail.com>
2019-10-23 20:49:15 +0300

    text in label is centered

--- label_pil.py
+++ label_pil.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw, ImageFilter, ImageEnhance, ImageFont
+from PIL import Image, ImageDraw, ImageFilter, ImageEnhance, ImageFont, ImageDraw
 
 
 def suggar_num_to_str(suggar):
@@ -27,20 +27,30 @@ def suggar_num_to_str_en(suggar):
     return suggar_str
 
 
+def find_middle(im, text, font):
+    draw = ImageDraw.Draw(im)
+    w, h = draw.textsize(text, font=font)
+    return int(w/2)
+
 def generate_label(title1, title2, number_alcohol, number_years, number_sweetness):
+    number_alcohol = "alc. " + str(round(number_alcohol, 1)) + " % by Vol."
+    number_sweetness = str(number_sweetness)
+    number_years = str(number_years)
+
     font_size = 20
+    font_offset = 20
     font_color = 'rgb(0, 0, 0)'
     font = ImageFont.truetype('fonts/Montserrat-Bold.otf', size=font_size)
     font_regular = ImageFont.truetype('fonts/Montserrat-Regular.otf', size=int(font_size))
     font_italic = ImageFont.truetype('fonts/Montserrat-Italic.otf', size=int(font_size / 1.2))
 
-    img = Image.new(mode="RGB", size=(200, 323), color=(255, 179, 179))
+    img = Image.new(mode="RGB", size=(200, 200), color=(255, 179, 179))
     x, y = img.size
 
     draw = ImageDraw.Draw(img)
-    draw.text((0, 0), title1, fill=font_color, font=font)
-    draw.text((0, 0 + font_size * 1), title2, fill=font_color, font=font_italic)
-    draw.text((0, 0 + font_size * 2), "alc. " + str(round(number_alcohol, 1)) +" % by Vol.", fill=font_color, font=font_regular)
-    draw.text((0, 0 + font_size * 3), str(number_sweetness), fill=font_color, font=font_regular)
-    draw.text((0, 0 + font_size * 4), str(number_years), fill=font_color, font=font_regular)
+    draw.text((x / 2 - find_middle(img, title1, font), 0), title1, fill=font_color, font=font)
+    draw.text((x / 2 - find_middle(img, title2, font_italic), 0 + font_size * 1 + font_offset), title2, fill=font_color, font=font_italic)
+    draw.text((x / 2 - find_middle(img, number_alcohol, font_regular), 0 + font_size * 2 + font_offset), number_alcohol, fill=font_color, font=font_regular)
+    draw.text((x / 2 - find_middle(img, number_sweetness, font_regular), 0 + font_size * 3 + font_offset), number_sweetness, fill=font_color, font=font_regular)
+    draw.text((x / 2 - find_middle(img, number_years, font_regular), 0 + font_size * 4 + font_offset), number_years, fill=font_color, font=font_regular)
     return img
